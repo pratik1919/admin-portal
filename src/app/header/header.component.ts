cdd68ebb202303a1e75655748936143b93828934
Aayush Regmi <aregmi@virmedica.com>
2019-06-20 10:22:16 +0545

    Angular 8 admin first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../../assets/styles/scss/components/_header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
