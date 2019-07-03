ca9eeeda19c131ca52a9221fa6c0791b93bfafed
Pratik Budhathoki <pratik@Pratiks-iMac.local>
2019-07-03 17:23:53 +0545

    Scss Structure

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['../../assets/styles/scss/components/_content.component.scss']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
