06054b1cba249cdd8382f8554673a46cea1208d8
Nikesh Maharjan <nmaharjan@virmedica.com>
2019-07-15 18:09:19 +0545

    Okta integeration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {OktaAuthGuard, OktaCallbackComponent} from '@okta/okta-angular';
+import {ContentComponent} from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ContentComponent, canActivate: [OktaAuthGuard]},
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OktaAuthService} from '@okta/okta-angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'oneconsole';
+  isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService, private router: Router) {
+    this.oktaAuth.$authenticationState.subscribe(isAuthenticated => this.isAuthenticated = isAuthenticated);
+  }
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  logout() {
+    this.oktaAuth.logout('/login');
+  }
 }
